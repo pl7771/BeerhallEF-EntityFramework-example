96ab2984a91ed6bc866a3e208d64c92f53d47a93
Valeriia <valeriia.mikhaylova@student.howest.be>
2020-10-10 15:01:58 +0200

    Add project files.

---
+++ BeerhallEF/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BeerhallEF.Data
+{
+    class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString =
+                @"Server=.;Database=Beerhall;Integrated Security=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
